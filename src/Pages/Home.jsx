aeec1731871fdce985f3a8d45b4aff2038579006
krystel <krystel.dev@gmail.com>
2022-08-23 20:37:46 +0200

    Fin

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,11 +3,10 @@ import Cliente from "../components/Cliente";
 
 const Home = () => {
   const [clientes, setclientes] = useState([]);
-
   useEffect(() => {
     const obtenercleintesAPI = async () => {
       try {
-        const url = "http://localhost:4000/clientes";
+        const url = import.meta.env.VITE_API_KEY;
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
 
@@ -24,7 +23,7 @@ const Home = () => {
 
     if (confirmar) {
       try {
-        const url = `http://localhost:4000/clientes/${clienteId}`;
+        const url = `${import.meta.env.VITE_API_KEY}/${clienteId}`;
         const respuesta = await fetch(url, {
           method: "DELETE",
         });
@@ -44,26 +43,32 @@ const Home = () => {
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
       <p className="mt-3">Administra tus clientes</p>
-
-      <table className="w-full mt-5 table-auto shadow bg-white">
-        <thead>
-          <tr className="bg-blue-800 text-white">
-            <th className="p-2">Nombre</th>
-            <th className="p-2">Contacto</th>
-            <th className="p-2">Empresa</th>
-            <th className="p-2">Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clientes.map((cliente) => (
-            <Cliente
-              key={cliente.id}
-              cliente={cliente}
-              handelEliminar={handelEliminar}
-            />
-          ))}
-        </tbody>
-      </table>
+      {clientes.length > 0 ? (
+        <table className="w-full mt-5 table-auto shadow bg-white">
+          <thead>
+            <tr className="bg-blue-800 text-white">
+              <th className="p-2">Nombre</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Empresa</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((cliente) => (
+              <Cliente
+                key={cliente.id}
+                cliente={cliente}
+                handelEliminar={handelEliminar}
+              />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>
+          No hay ningún cliente registrado. añada un registro clicando en Nuevo
+          Cliente
+        </p>
+      )}
     </>
   );
 };
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -30,7 +30,7 @@ const Formulario = ({ cliente, cargando, setCargando }) => {
       let respuesta;
       if (cliente.id) {
         //Actualizar cliente
-        const url = `http://localhost:4000/clientes/${cliente.id}`;
+        const url = `${import.meta.env.VITE_API_KEY}/${cliente.id}`;
 
         respuesta = await fetch(url, {
           method: "PUT",
@@ -41,7 +41,7 @@ const Formulario = ({ cliente, cargando, setCargando }) => {
         });
       } else {
         //nuevo registro
-        const url = "http://localhost:4000/clientes";
+        const url = `${import.meta.env.VITE_API_KEY}`;
 
         respuesta = await fetch(url, {
           method: "POST",
